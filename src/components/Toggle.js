025d7bd13a27ccb83627ff2b4afb455907ddf08c
Michael Warren <mwarrn@amazon.com>
2022-06-18 14:02:29 -0700

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  // To create a state variable in our component, we need to call useState
+  // and provide an initial value
+  const [isOn, setIsOn] = useState(false);
+
+  // Call the setter function to update our state variable
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  // Use the isOn variable to determine what text to display in the button and its color
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
